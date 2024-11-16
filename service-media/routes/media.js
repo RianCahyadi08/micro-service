8f5c18e075bdb1fa9346c8b286668f58c7a3db64
Rian Cahyadi <ryancahyadi11@gmail.com>
2024-11-16 23:27:04 +0700

    created api get media created api delete media renamed folder dir media

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -2,8 +2,37 @@ const express = require("express");
 const router = express.Router();
 const isBase64 = require("is-base64");
 const base64Img = require("base64-img");
-
 const { Media } = require("../models");
+const fs = require("fs");
+
+router.get("/", (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+
+  const offset = (page - 1) * limit;
+
+  Media.findAndCountAll({
+    offset,
+    limit,
+  })
+    .then((result) => {
+      return res.json({
+        status: "success",
+        data: result.rows.map((media) => ({
+          id: media.id,
+          image: `${req.get("host")}/${media.image}`,
+        })),
+        totalPages: Math.ceil(result.count / limit),
+        currentPage: page,
+      });
+    })
+    .catch((err) => {
+      return res.status(400).json({
+        status: "error",
+        message: err.message,
+      });
+    });
+});
 
 router.post("/", (req, res) => {
   const image = req.body.image;
@@ -15,7 +44,47 @@ router.post("/", (req, res) => {
     });
   }
 
-  base64Img.img(image, "./public/images/", Date.now(), async (err, filePath) => {
+  base64Img.img(
+    image,
+    "./public/images/",
+    Date.now(),
+    async (err, filePath) => {
+      if (err) {
+        return res.status(400).json({
+          status: "error",
+          message: err.message,
+        });
+      }
+
+      const fileName = filePath.split("\\").pop().split("/").pop();
+
+      const media = await Media.create({ image: `images/${fileName}` });
+
+      return res.json({
+        status: "success",
+        message: "Image uploaded successfully",
+        data: {
+          id: media.id,
+          image: `${req.get("host")}/images/${fileName}`,
+        },
+      });
+    }
+  );
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const media = await Media.findByPk(id);
+
+  if (!media) {
+    return res.status(404).json({
+      status: "error",
+      message: "Media not found",
+    });
+  }
+
+  fs.unlink(`./public/${media.image}`, async (err) => {
     if (err) {
       return res.status(400).json({
         status: "error",
@@ -23,17 +92,11 @@ router.post("/", (req, res) => {
       });
     }
 
-    const fileName = filePath.split("\\").pop().split("/").pop();
-
-    const media = await Media.create({ image: `image/${fileName}` });
+    await media.destroy();
 
     return res.json({
       status: "success",
-      message: "Image uploaded successfully",
-      data: {
-        id: media.id,
-        image: `${req.get('host')}/images/${fileName}`,
-      },
+      message: "Media deleted successfully",
     });
   });
 });

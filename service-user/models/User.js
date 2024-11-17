9d466eb298b8480b8f9dca9c08cd62d53e0dd6b2
Rian Cahyadi <ryancahyadi11@gmail.com>
2024-11-17 17:37:26 +0700

    Create model user

---
+++ service-user/models/User.js
@@ -0,0 +1,53 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        role: {
+            type: DataTypes.ENUM,
+            value: ['admin', 'student'],
+            allowNull: false,
+            defaultValue: 'student',
+        },
+        avatar: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        profession: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        createdAt: {
+            field: 'created_at',
+            type: DataTypes.DATE,
+        },
+        updatedAt: {
+            field: 'updated_at',
+            type: DataTypes.DATE,
+        },
+    }, {
+        tableName: 'users',
+        timestamps: true,
+    });
+
+    return User;
+}

fc80ac62e17aadca9fdceb572c622b676fc1b0ae
RianCahyadi08 <ryancahyadi11@gmail.com>
2024-11-26 22:00:18 +0700

    Create api get all users and user

--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const usersHandler = require('./handler/users');
+const usersHandler = require("./handler/users");
 
-router.post('/register', usersHandler.register);
-router.post('/login', usersHandler.login);
-router.put('/:id', usersHandler.update);
+router.post("/register", usersHandler.register);
+router.post("/login", usersHandler.login);
+router.put("/:id", usersHandler.update);
+router.get("/:id", usersHandler.getUser);
+router.get("/", usersHandler.getUsers);
 
 module.exports = router;

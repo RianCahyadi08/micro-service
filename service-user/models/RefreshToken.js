8c1a1a1a0d0272dd168c8416f12f74cf11e40468
RianCahyadi08 <ryancahyadi11@gmail.com>
2024-11-26 21:59:36 +0700

    Create model refresh token

--- service-user/models/RefreshToken.js
+++ service-user/models/RefreshToken.js
@@ -1,33 +1,38 @@
 module.exports = (sequelize, DataTypes) => {
-    const RefreshToken = sequelize.define('RefreshToken', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
-        },
-        token: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        userId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        createdAt: {
-            type: DataTypes.DATE,
-            field: 'created_at',
-            allowNull: false,
-        },
-        updatedAt: {
-            type: DataTypes.DATE,
-            field: 'updated_at',
-            allowNull: false,
-        }
-    }, {
-        tableName: 'refresh_tokens',
-        timestamps: true
-    });
+  const RefreshToken = sequelize.define(
+    "RefreshToken",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      token: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        field: "user_id",
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        field: "created_at",
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        field: "updated_at",
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "refresh_tokens",
+      timestamps: true,
+    }
+  );
 
-    return RefreshToken;
-}
+  return RefreshToken;
+};
